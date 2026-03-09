ed9d5debd693f05979ed2d9ab58c7c1dd29651cf
Dystian En <distian.enyus@gmail.com>
2026-03-09 19:16:03 +0700

    remove config

--- config.js
+++ config.js
@@ -6,18 +6,15 @@ module.exports = {
   microsoftUrl:
     "https://www.microsoft.com/en-us/microsoft-365/business/microsoft-365-business-basic",
   adsPower: {
-    baseUrl: process.env.ADSPOWER_BASE_URL || "http://local.adspower.net:50325",
-    apiKey:
-      process.env.ADSPOWER_API_KEY ||
-      "a74c1696169a859321288618723ac1340051adfbdc2511a1",
-    groupId: process.env.ADSPOWER_GROUP_ID || "0",
+    baseUrl: process.env.ADSPOWER_BASE_URL,
+    apiKey: process.env.ADSPOWER_API_KEY,
+    groupId: process.env.ADSPOWER_GROUP_ID,
   },
   proxy: {
-    host: process.env.PROXY_HOST || "gw.dataimpulse.com",
-    port: process.env.PROXY_PORT || "824",
-    username:
-      process.env.PROXY_USERNAME || "2028ce76fd49cc77d009__cr.fi_rotate",
-    password: process.env.PROXY_PASSWORD || "2daec922ca463aaa",
+    host: process.env.PROXY_HOST,
+    port: process.env.PROXY_PORT,
+    username: process.env.PROXY_USERNAME,
+    password: process.env.PROXY_PASSWORD,
     type: "socks5",
   },
 };
