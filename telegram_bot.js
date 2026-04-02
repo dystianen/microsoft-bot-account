f8840d1fe9e85e38ed7e7e44e12c9fc42936bb68
Dystian En <distian.enyus@gmail.com>
2026-04-02 09:18:37 +0700

    oke

--- telegram_bot.js
+++ telegram_bot.js
@@ -101,7 +101,7 @@ function initializeBotHandlers(bot) {
 
     bot.sendMessage(
       chatId,
-      "Welcome to Microsoft Bott! 🤖 (MongoDB Powered)\n\nAdd accounts to the session, and they will be cleared once processed or manually reset.",
+      "Welcome to Microsoft Bot! 🤖 (MongoDB Powered)\n\nAdd accounts to the session, and they will be cleared once processed or manually reset.",
       mainMenu,
     );
   });
